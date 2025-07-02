a87142375be179fc448a895d68ee290e124cd4c2
mori calisir <calisirmori@gmail.com>
2025-07-01 18:30:13 -0700

    int

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '3rem' }}>
+      <h1>🏀 CourtVision Frontend (Vite)</h1>
+      <p>This is a fast dummy React app</p>
+    </div>
+  );
+}
+
+export default App;
